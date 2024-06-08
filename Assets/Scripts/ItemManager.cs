35796b5f9b08da9b410eed64d8c9b0cfb8eb942e
YeDemirkiran <yedemirkiran27@gmail.com>
2024-06-08 21:30:02 +0300

    Add UI for item shop

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] GameObject itemStripPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
